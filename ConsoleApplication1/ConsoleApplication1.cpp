a063194e6ed2c0729f170769e65a721326747e38
Ravi Xavier <ravixavier.dev@gmail.com>
2024-04-12 15:29:58 -0300

    class and objects; + access modifier;

--- ConsoleApplication1/ConsoleApplication1.cpp
+++ ConsoleApplication1/ConsoleApplication1.cpp
@@ -1,5 +1,23 @@
 #include <iostream>
+using std::string;
+
+class Employee {
+public:
+	string Name;
+	string Company;
+	int Age;
+
+	void IntroduceYourself() {
+		std::cout << "Name - " << Name << std::endl;
+		std::cout << "Company - " << Company << std::endl;
+		std::cout << "Age - " << Age << std::endl;
+	}
+};
 
 int main() {
-
+	Employee employee1;
+	employee1.Name = "Ravi";
+	employee1.Company = "ABCCompany";
+	employee1.Age = 28;
+	employee1.IntroduceYourself();
 }

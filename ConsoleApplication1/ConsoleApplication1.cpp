3975c01d1daf59968f93e613a72f5c7b08ebea59
Ravi Xavier <ravixavier.dev@gmail.com>
2024-04-12 16:59:39 -0300

    constructors

--- ConsoleApplication1/ConsoleApplication1.cpp
+++ ConsoleApplication1/ConsoleApplication1.cpp
@@ -12,12 +12,24 @@ public:
 		std::cout << "Company - " << Company << std::endl;
 		std::cout << "Age - " << Age << std::endl;
 	}
+
+	Employee(string name, string company, int age) {
+		Name = name;
+		Company = company;
+		Age = age;
+	}
 };
 
 int main() {
-	Employee employee1;
-	employee1.Name = "Ravi";
+	Employee employee1 = Employee("Ravi", "ABCCompany", 28);
+	/*employee1.Name = "Ravi";
 	employee1.Company = "ABCCompany";
-	employee1.Age = 28;
+	employee1.Age = 28;*/
 	employee1.IntroduceYourself();
+
+	Employee employee2 = Employee("David", "Microsoft", 36);
+	employee2.IntroduceYourself();
+
+	Employee employee3 = Employee("Ron", "Google", 38);
+	employee3.IntroduceYourself();
 }

dcae47214c068a0c7142e8f8b77e845e47a6cb89
Ravi Xavier <ravixavier.dev@gmail.com>
2024-04-15 14:15:37 -0300

    encapsulation, setters and getters.

--- ConsoleApplication1/ConsoleApplication1.cpp
+++ ConsoleApplication1/ConsoleApplication1.cpp
@@ -2,11 +2,12 @@
 using std::string;
 
 class Employee {
-public:
+private:
 	string Name;
 	string Company;
 	int Age;
 
+public:
 	void IntroduceYourself() {
 		std::cout << "Name - " << Name << std::endl;
 		std::cout << "Company - " << Company << std::endl;
@@ -18,13 +19,42 @@ public:
 		Company = company;
 		Age = age;
 	}
+
+	//setter
+	void setName(string name) {
+		Name = name;
+	}
+	//getter
+	string getName() {
+		return Name;
+	}
+
+	//setter
+	void setCompany(string company) {
+		Company = company;
+	}
+	//getter
+	string getCompany() {
+		return Company;
+	}
+
+	//setter
+	void setAge(int age) {
+		if (age >= 18)
+		Age = age;
+	}
+	//getter
+	int getAge() {
+		return Age;
+	}
+
 };
 
 int main() {
-	Employee employee1 = Employee("Ravi", "ABCCompany", 28);
+	Employee employee1 = Employee("Ravi", "ABCCompany", 27);
 	/*employee1.Name = "Ravi";
 	employee1.Company = "ABCCompany";
-	employee1.Age = 28;*/
+	employee1.Age = 27;*/
 	employee1.IntroduceYourself();
 
 	Employee employee2 = Employee("David", "Microsoft", 36);
@@ -32,4 +62,8 @@ int main() {
 
 	Employee employee3 = Employee("Ron", "Google", 38);
 	employee3.IntroduceYourself();
+
+	//testing setters and getters
+	employee1.setAge(16);
+	std::cout << employee1.getName() << " is " << employee1.getAge() << " years old." << std::endl;
 }

db01acce67880e0d9a425894d4b256f8c9c3eea9
Ravi Xavier <ravixavier.dev@gmail.com>
2024-04-16 13:26:37 -0300

    inheritance

--- ConsoleApplication1/ConsoleApplication1.cpp
+++ ConsoleApplication1/ConsoleApplication1.cpp
@@ -2,29 +2,20 @@
 using std::string;
 
 class AbstractEmployee {
-    virtual void AskForPromotion()=0;
+	virtual void AskForPromotion() = 0;
 };
 
-class Employee:AbstractEmployee {
+class Employee :AbstractEmployee {
+
 private:
-	string Name;
 	string Company;
 	int Age;
 
+protected:
+	string Name;
+
 public:
-	void IntroduceYourself() {
-		std::cout << "Name - " << Name << std::endl;
-		std::cout << "Company - " << Company << std::endl;
-		std::cout << "Age - " << Age << std::endl;
-	}
-
-	Employee(string name, string company, int age) {
-		Name = name;
-		Company = company;
-		Age = age;
-	}
-
-    //name setter&getter
+	//name setter&getter
 	void setName(string name) {
 		Name = name;
 	}
@@ -43,29 +34,77 @@ public:
 	//age setter&getter
 	void setAge(int age) {
 		if (age >= 18)
-		Age = age;
+			Age = age;
 	}
 	int getAge() {
 		return Age;
 	}
-	
 
-void AskForPromotion() {
-    if(Age>=30)
-    std::cout << Name << " got promoted!" << std::endl;
-    else
-        std::cout << Name << ", sorry NO promotion for you!" << std::endl;
-}
+	void IntroduceYourself() {
+		std::cout << "Name - " << Name << std::endl;
+		std::cout << "Company - " << Company << std::endl;
+		std::cout << "Age - " << Age << std::endl;
+	}
+
+	Employee(string name, string company, int age) {
+		Name = name;
+		Company = company;
+		Age = age;
+	}
+
+
+
+	void AskForPromotion() {
+		if (Age >= 30)
+			std::cout << Name << " got promoted!" << std::endl;
+		else
+			std::cout << Name << ", sorry NO promotion for you!" << std::endl;
+	}
+
+};
+
+class Developer : public Employee {
+public:
+	string FavProgrammingLanguage;
+	Developer(string name, string company, int age, string favProgrammingLanguage) :Employee(name, company, age)
+	{
+		FavProgrammingLanguage = favProgrammingLanguage;
+	}
+	void FixBug() {
+		std::cout << Name << " fixed bug using " << FavProgrammingLanguage << std::endl;
+	}
+};
+
+class Teacher : public Employee {
+public:
+	string Subject;
+	Teacher(string name, string company, int age, string subject) :Employee(name, company, age) {
+		Subject = subject;
+	}
+	void PrepareLesson() {
+		std::cout << Name << " is preparing " << Subject << " lesson." << std::endl;
+	}
 
 };
 
 int main() {
-	Employee employee1 = Employee("Ravi", "ABCCompany", 30);
+	Employee employee1 = Employee("Ravi", "Microsoft", 30);
 	Employee employee2 = Employee("Ruy", "Amazon", 28);
 	Employee employee3 = Employee("Michael", "Google", 25);
 
+	Developer d = Developer("Ravi", "Microsoft", 30, "C#.");
+	d.FixBug();
+	d.FixBug();
+	d.FixBug();
+	d.AskForPromotion();
+
+	Teacher t = Teacher("John", "UFBA", 30, "History");
+	t.PrepareLesson();
+	t.AskForPromotion();
+
+
 	/*
-    employee1.Name = "Ravi";
+	employee1.Name = "Ravi";
 	employee1.Company = "ABCCompany";
 	employee1.Age = 27;
 
@@ -76,14 +115,17 @@ int main() {
 	employee2.IntroduceYourself();
 	employee3.IntroduceYourself();
 
-    
+
 	testing setters and getters
 	employee1.setAge(16);
 	std::cout << employee1.getName() << " is " << employee1.getAge() << " years old." << std::endl;
-    */
 
-   employee1.AskForPromotion();
-   employee2.AskForPromotion();
-   employee3.AskForPromotion();
+
+	employee1.AskForPromotion();
+	employee2.AskForPromotion();
+	employee3.AskForPromotion();
+   */
+
+
 
 }
